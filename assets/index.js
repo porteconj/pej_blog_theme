e15a42159c790e0ab0b1f51b3e143ec2ded0fd8e
Olaf Ghanizadeh <laffen91@gmail.com>
2020-04-05 12:13:47 +0100

    parent-fit

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,11 @@ import "./css/styles.css";
 import "./js/nojs.js"
 
 import 'lazysizes';
+import 'lazysizes/plugins/object-fit/ls.object-fit';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import 'lazysizes/plugins/blur-up/ls.blur-up';
+
+
 
 
 var Turbolinks = require("turbolinks")
